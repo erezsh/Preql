c420af6c613dbcd43c5d3ae915073f8fbb7c0e90
Erez Shinan <erezshin+git@gmail.com>
2019-06-02 18:25:33 +0300

    Added tree_test

---
+++ preql/tree_test.py
@@ -0,0 +1,16 @@
+
+from . import Preql
+
+def main():
+    preql = Preql()
+    preql.load('tree.pql', rel_to=__file__)
+
+    print( list(preql('Tree' ) ) )
+    print( list(preql('Tree {value, parent.value}' ) ) )
+    # print( list(preql('Person {name, city.country.name}' ) ) )
+
+    # print( list(preql('City {country.name => citizens}' ) ) )
+
+    preql.start_repl()
+
+main()
