bae000fd81fb80aeee199c18300557a609f5b278
Tal Shiri <eiopaa@gmail.com>
2020-06-17 23:37:45 +0300

    .preql support

--- preql/__main__.py
+++ preql/__main__.py
@@ -1,10 +1,25 @@
 import argparse
 from . import Preql
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Preql command-line interface')
 parser.add_argument('-i', '--interactive', action='store_true', help="Enter interactive mode after running the script")
 parser.add_argument('-d', '--debug', action='store_true', help="Display debug information")
 parser.add_argument('script_path', type=str, nargs='?', default=None, help='Path to a Preql script to run')
+
+def find_dot_preql():
+    current_dir = Path.cwd()
+    while True:
+        dot_preql = current_dir / ".preql"
+        if dot_preql.exists():
+            return dot_preql
+
+        if current_dir.is_mount():
+            return
+
+        current_dir = current_dir.parent
+
+
 
 def main():
     args = parser.parse_args()
@@ -17,6 +32,10 @@ def main():
         with open(args.script_path) as f:
             p.run_code(f.read(), args.script_path)
     else:
+        dot_preql = find_dot_preql()
+        if dot_preql:
+            p.run_code(dot_preql.read_text(), dot_preql)
+
         interactive = True
 
     if interactive:
