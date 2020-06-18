b383a26695c08e15ba16a1f5cf3fe4d62781756a
Tal Shiri <eiopaa@gmail.com>
2020-06-18 10:02:03 +0300

    [cli] more consistent with psql and mysql cli
    - the nargs argument is now the database connection
    - moved running a script to -f

--- preql/__main__.py
+++ preql/__main__.py
@@ -5,7 +5,8 @@ from pathlib import Path
 parser = argparse.ArgumentParser(description='Preql command-line interface')
 parser.add_argument('-i', '--interactive', action='store_true', help="Enter interactive mode after running the script")
 parser.add_argument('-d', '--debug', action='store_true', help="Display debug information")
-parser.add_argument('script_path', type=str, nargs='?', default=None, help='Path to a Preql script to run')
+parser.add_argument('-f', '--file', type=str, help='Path to a Preql script to run')
+parser.add_argument('database', type=str, nargs='?', default=None, help="database url (postgres://user:password@host:port/db_name")
 
 def find_dot_preql():
     current_dir = Path.cwd()
@@ -25,12 +26,14 @@ def main():
     args = parser.parse_args()
 
     p = Preql(debug=args.debug)
+    if args.database:
+        p.interp.state.connect(args.database)
 
     interactive = args.interactive
 
-    if args.script_path:
-        with open(args.script_path) as f:
-            p.run_code(f.read(), args.script_path)
+    if args.file:
+        with open(args.file) as f:
+            p.run_code(f.read(), args.file)
     else:
         dot_preql = find_dot_preql()
         if dot_preql:
