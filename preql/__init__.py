1f3145e6e08a29648fd2117a1f3ce9fab0dac544
Erez Sh <erezshin@gmail.com>
2020-11-02 17:22:41 +0200

    Change version to manual string for now

--- preql/__init__.py
+++ preql/__init__.py
@@ -3,5 +3,6 @@ from . import _base_imports
 from .api import Preql
 from .exceptions import Signal
 
-import importlib.metadata as importlib_metadata
-__version__ = importlib_metadata.version("prql")
+# import importlib.metadata as importlib_metadata
+# __version__ = importlib_metadata.version("prql")
+__version__ = "0.1.11"
